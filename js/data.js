93749ecbddcb5caa889dcd42ce28a1b01895eb43
Nate Smith <nate.smith@coxinc.com>
2012-08-11 16:54:30 -0400

    restructure, xui->jquery, split stuff out

---
+++ js/data.js
@@ -0,0 +1,52 @@
+var data = [{
+    category: "Vegan ",
+    places: [{
+        name: "Dough Bakery",
+        price: "$",
+        address: "100 Hurt St NE 30307; (404) 380-1400",
+        notes: "cookies, pie, sticky buns, soft-serve, milkshakes"
+    }, {
+        name: "Dulce Vegan Bakery & Cafe",
+        price: "$",
+        address: "1994 Hosea L Williams Dr 30317; (404) 624-7417",
+        notes: "cupcakes, pastries, breakfast sandwiches, lunch"
+    }]
+}, {
+    category: "Vegetarian ",
+    places: [{
+        name: "Cafe Sunflower - two locations",
+        price: "$$",
+        address: "2140 Peachtree Rd; (404) 352-8859",
+        notes: "New American upscale veg dining, good for dates.<br />5975 Roswell Rd; (404) 256-1675 Family-friendly"
+    }, {
+        name: "Chat Patti Indian Vegetarian",
+        price: "$",
+        address: "1594 Woodcliff Dr NE # F 30329;  (404)633-5595",
+        notes: "Ask for the dairy-free thali"
+    }]
+}, {
+    category: "Vegan Friendly ",
+    places: [{
+        name: "Bahel Ethiopian Restaurant",
+        price: "$$",
+        address: "3125 Briarcliff Rd NE 30329; (404) 325-6000",
+        notes: "veg dishes are prepared w/ corn oil, not butter"
+    }, {
+        name: "Blossom Tree",
+        price: "$",
+        address: "64 Peachtree St NW 30303; (404) 223-7500",
+        notes: "Korean tofu tacos, fried sweet potatoes"
+    }]
+}, {
+    category: "Grocery",
+    places: [{
+        name: "Buford Highway Farmer's Market",
+        address: "5600 Buford Hwy NE 30340; (770) 455-0770",
+        notes: "massive international market, Korean cafe in back"
+    }, {
+        name: "Rainbow Grocery",
+        price: "$",
+        address: "2118 N Decatur Rd 30033; (404) 636-5553",
+        notes: "full grocery with mostly-vegan hot bar"
+    }]
+}]; // being a list of columns
