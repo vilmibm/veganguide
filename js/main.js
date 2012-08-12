a433ebdb685819bcd566810cb86b8ef7e4b2d1a7
Nate Smith <nate.smith@coxinc.com>
2012-08-12 17:09:23 -0400

    add comments, split js into main.js

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// data is provided by data.js. See that file for its structure.
+
+// add map function
+data.forEach(function(column) {
+    column.places.forEach(function(p) {
+        // gutenfleischers, mysore woodlands, graveyard, revolution, spoon west, we suki suki, arden's, tradjoe, wf
+        p.map = function() {
+            return (this.name+' '+this.address).replace(/ /g, '+');
+        };
+    });
+});
+
+// lay out data
+var column_tmpl = $('#column.template').html();
+$('#content div.row-fluid').html(data.reduce(function(p, c) {
+    return p + Mustache.render(column_tmpl, c);
+}, ""));
+
+// suggestion form
+$('#suggest form').submit(function(e) {
+    e.preventDefault();
+    $.post('http://localhost:3000/suggest', $("#suggest form").serialize())
+    .success(function() {
+        $('#suggest').modal('hide');
+        $('#hooray').modal('show');
+    })
+    .error(function() { $('#tryagain').modal('show'); });
+});
+
+// filtering
+$('#filter').keyup(function() {
+    var search = $('#filter').attr('value');
+    console.log(search);
+    if (search === '') {
+        $('.place').show();
+    }
+    else {
+        $('.place').each(function() {
+            var $place = $(this);
+            var selectors = ['.address', '.name', '.notes'];
+            var match = selectors.reduce(function(p,c) {
+                return p || Boolean($place.find(c).text().toLowerCase().match(search));
+            }, false);
+            if (!match) { $place.hide(); }
+            else { $place.show(); }
+        });
+    }
+});
+
+// random
+$('#random').click(function() {
+    $('.highlight').removeClass('highlight');
+    var $places = $('.place:visible')
+    var index = Math.floor(Math.random() * ($places.length + 1));
+    var $place = $($places[index]);
+    $place.addClass('highlight');
+    $('html, body').animate({scrollTop: $place.offset().top-$place.height()}, 500);
+});
